31312a3cc08e2d3ccf28f901845e85a737bb1e31
Mika Li <mikali6789@gmail.com>
2023-01-30 11:48:48 +0800

    Add application class
    - register the application class, so the entire project
    is aware, hence, add the name of the application class
    inside the app module AndroidManifest.xml

---
+++ app/src/main/java/com/mikali/weathermemoir/WeatherMemoirApplication.kt
@@ -0,0 +1,8 @@
+package com.mikali.weathermemoir
+
+import android.app.Application
+
+/* The application class is instantiated before any other classes when an application starts.
+This is a good place to setup koin dependency injection.
+* */
+class WeatherMemoirApplication : Application()
