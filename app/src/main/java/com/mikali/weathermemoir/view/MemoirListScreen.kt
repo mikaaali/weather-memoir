68268917a8cdf31d4675b01156f761c58239c2ce
Mika Li <mikali6789@gmail.com>
2023-01-31 21:47:52 +0800

    Create skeleton UI for all the other screens
    So we can use the UI in our navigation setup.
    But the detail UI creation will happen in
    a later commits

---
+++ app/src/main/java/com/mikali/weathermemoir/view/MemoirListScreen.kt
@@ -0,0 +1,12 @@
+package com.mikali.weathermemoir.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun MemoirListScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+}
