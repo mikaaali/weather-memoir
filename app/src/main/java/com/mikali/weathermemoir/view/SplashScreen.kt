cd1023c94f497c4a9b73cadda49f048df80c052a
Mika Li <mikali6789@gmail.com>
2023-01-31 20:54:04 +0800

    Create splash screen UI
    - use `brush = Brush.linearGradient(listOf(colors))`
    to give the border an extra gradiant
    
    - apply different .scale value to give the animation effect
    
    - LaunchedEffect is a side effect handler that allow us
    to perform other logic in the backend and not block the
    UI, which is performed inside a coroutine
    
    - image logo is stored in the res file
    
    - take in a navigation parameter because we want to
    use that to navigate to other screens

---
+++ app/src/main/java/com/mikali/weathermemoir/view/SplashScreen.kt
@@ -0,0 +1,77 @@
+package com.mikali.weathermemoir.view
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.mikali.weathermemoir.R
+import com.mikali.weathermemoir.view.theme.Cyan
+import com.mikali.weathermemoir.view.theme.Green
+import com.mikali.weathermemoir.view.theme.LightGreen
+import com.mikali.weathermemoir.view.theme.Teal
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(
+        key1 = true,
+        block = {
+            scale.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(
+                    durationMillis = 1200,
+                    easing = {
+                        OvershootInterpolator().getInterpolation(it)
+                    }
+                )
+            )
+            delay(1500)
+        }
+    )
+
+    Box(modifier.fillMaxSize()) {
+        Surface(
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .size(size = 380.dp)
+                .scale(scale = scale.value),
+            shape = CircleShape,
+            color = Color.White,
+            border = BorderStroke(
+                width = 16.dp,
+                brush = Brush.linearGradient(listOf(Cyan, Teal, Green, LightGreen))
+            )
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.splash),
+                contentScale = ContentScale.Inside,
+                contentDescription = "splash logo"
+            )
+        }
+    }
+}
