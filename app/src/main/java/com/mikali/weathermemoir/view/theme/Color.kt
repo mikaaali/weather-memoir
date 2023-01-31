e6161fffb5c7f15238c10e8c986b1cb460c510cf
Mika Li <mikali6789@gmail.com>
2023-01-31 19:37:59 +0800

    Update Color and Theme for this app

--- app/src/main/java/com/mikali/weathermemoir/view/theme/Color.kt
+++ app/src/main/java/com/mikali/weathermemoir/view/theme/Color.kt
@@ -2,7 +2,7 @@ package com.mikali.weathermemoir.view.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Cyan = Color(0xFF00bcd4)
+val Teal = Color(0xFF009688)
+val Green = Color(0xFF4caf50)
+val LightGreen = Color(0xFF8bc34a)
--- app/src/main/java/com/mikali/weathermemoir/view/theme/Theme.kt
+++ app/src/main/java/com/mikali/weathermemoir/view/theme/Theme.kt
@@ -5,26 +5,25 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = LightGreen,
+    primaryVariant = Green,
+    secondary = Teal
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = LightGreen,
+    primaryVariant = Green,
+    secondary = Teal,
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSurface = Color.Black
+
 )
 
 @Composable
