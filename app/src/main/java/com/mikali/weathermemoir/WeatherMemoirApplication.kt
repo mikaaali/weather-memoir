ea7d815f2aa73fd11457d77b5c41ebefa9658e52
Mika Li <mikali6789@gmail.com>
2023-02-01 20:42:53 +0800

    Add `FirebaseApp.initializeApp(this)`
    Because we already added the firebase
    analytic dependency, hence the app
    will run into the an initializes Firebase Analytics
    error if we try to use any other firebase feature.

--- app/src/main/java/com/mikali/weathermemoir/WeatherMemoirApplication.kt
+++ app/src/main/java/com/mikali/weathermemoir/WeatherMemoirApplication.kt
@@ -1,8 +1,16 @@
 package com.mikali.weathermemoir
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 
 /* The application class is instantiated before any other classes when an application starts.
-This is a good place to setup koin dependency injection.
+This is a good place to setup koin dependency injection, start
 * */
-class WeatherMemoirApplication : Application()
+class WeatherMemoirApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseApp.initializeApp(this)
+    }
+}
