939d65dbe44b5a1d456225d704bba2eb0c1963b4
Mika Li <mikali6789@gmail.com>
2023-02-03 12:50:44 +0800

    Start Koin

--- app/src/main/java/com/mikali/weathermemoir/WeatherMemoirApplication.kt
+++ app/src/main/java/com/mikali/weathermemoir/WeatherMemoirApplication.kt
@@ -2,15 +2,28 @@ package com.mikali.weathermemoir
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.mikali.weathermemoir.di.appModule
+import com.mikali.weathermemoir.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 /* The application class is instantiated before any other classes when an application starts.
-This is a good place to setup koin dependency injection, start
+This is a good place to setup koin dependency injection, this way all the created objects will
+be available to all classes.
 * */
 class WeatherMemoirApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        // initialize firebase analytics
         FirebaseApp.initializeApp(this)
+
+        startKoin {
+            androidLogger()
+            androidContext(this@WeatherMemoirApplication)
+            modules(listOf(networkModule, appModule))
+        }
     }
 }
