299f1032843a02843ebb3afe4763554150ca6e51
Mika Li <mikali6789@gmail.com>
2023-02-04 23:22:42 +0800

    Define MainViewModel instance for dependency injection later

--- app/src/main/java/com/mikali/weathermemoir/di/AppModule.kt
+++ app/src/main/java/com/mikali/weathermemoir/di/AppModule.kt
@@ -2,13 +2,23 @@ package com.mikali.weathermemoir.di
 
 import com.mikali.weathermemoir.repository.WeatherRepository
 import com.mikali.weathermemoir.repository.WeatherRepositoryImpl
+import com.mikali.weathermemoir.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
 
+    // define weatherRepository instance
     single<WeatherRepository> {
         WeatherRepositoryImpl(
             weatherAPI = get()
+        )
+    }
+
+    // define MainViewModel instance
+    viewModel {
+        MainViewModel(
+            weatherRepository = get()
         )
     }
 }
