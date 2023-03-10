8b68236c37338dae15bba7ad87bf101b1c7655a5
Mika Li <mikali6789@gmail.com>
2023-03-10 13:45:54 -0500

    Update the UserInfo data class
    So it can be serialized from runtime object to
    a format that can be transferred over network,
    also deserializable from network format to kotlin runtime
    object

--- app/src/main/java/com/mikali/weathermemoir/model/UserInfo.kt
+++ app/src/main/java/com/mikali/weathermemoir/model/UserInfo.kt
@@ -1,25 +1,31 @@
 package com.mikali.weathermemoir.model
 
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.PropertyName
+
 data class UserInfo(
-    val uid: String,
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val memoirList: List<Memoir>
+    @Exclude
+    @get:PropertyName("firestore_uid")
+    @set:PropertyName("firestore_uid")
+    var firestoreUid: String? = null,
+    @get:PropertyName("auth_uid")
+    @set:PropertyName("auth_uid")
+    var authUid: String? = null,
+    @get:PropertyName("first_name")
+    @set:PropertyName("first_name")
+    var firstName: String? = null,
+    @get:PropertyName("last_name")
+    @set:PropertyName("last_name")
+    var lastName: String? = null,
+    @get:PropertyName("memoir_list")
+    @set:PropertyName("memoir_list")
+    var memoirList: List<Memoir>? = emptyList()
 ) {
-
     data class Memoir(
-        val question: String,
-        val thought: String,
-        val creationTime: String,
-        val mainWeatherConditionDescription: String
-    )
-
-    fun toMutableMap(): MutableMap<String, Any> = mutableMapOf(
-        "uid" to this.uid,
-        "first_name" to this.firstName,
-        "last_name" to this.lastName,
-        "email" to this.email,
-        "memoir_list" to this.memoirList
+        val question: String = "",
+        val thought: String = "",
+        val creationTime: String = "",
+        val mainWeatherConditionIcon: String = "",
+        val mainWeatherConditionDescription: String = ""
     )
 }
