c2760f00aad84145ecb6db24c49073c88952999a
Mika Li <mikali6789@gmail.com>
2023-03-10 14:10:54 -0500

    Add const value that will be used later

--- app/src/main/java/com/mikali/weathermemoir/util/Constants.kt
+++ app/src/main/java/com/mikali/weathermemoir/util/Constants.kt
@@ -1,9 +1,18 @@
 package com.mikali.weathermemoir.util
 
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+
 object Constants {
     const val API_KEY = "2393f5f9639467c6131b3cdf4b2e7516"
-    const val MAIN_TAG = "MainViewModel"
+    const val HOME_TAG = "HomeViewModel"
     const val LOGIN_TAG = "LoginViewModel"
     const val SIGNUP_TAG = "SignupViewModel"
-    const val Questionnaire_TAG = "QuestionnaireViewModel"
+    const val QUESTIONNAIRE_TAG = "QuestionnaireViewModel"
+    const val MEMOIR_LIST_TAG = "MemoirListViewModel"
+    const val MAIN_TAG = "MainViewModel"
+
+    // ex. 3/26/22, 10:00 AM
+    val dateTimeFormatter: DateTimeFormatter =
+        DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
 }
