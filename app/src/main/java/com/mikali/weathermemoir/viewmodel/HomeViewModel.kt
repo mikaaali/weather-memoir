54b1cbc505fb7308e033e72600e7ea9163a2ce81
Mika Li <mikali6789@gmail.com>
2023-03-10 14:30:11 -0500

    Modify homeViewModel to hold a data class of object instead of just weather type

--- app/src/main/java/com/mikali/weathermemoir/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/mikali/weathermemoir/viewmodel/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.mikali.weathermemoir.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.mikali.weathermemoir.model.UserInfo
 import com.mikali.weathermemoir.model.weather.Weather
 import com.mikali.weathermemoir.repository.WeatherRepository
 import com.mikali.weathermemoir.util.Constants
@@ -19,13 +20,30 @@ class HomeViewModel(
     private val compositeDisposable = CompositeDisposable()
 
     // Mutable, such that new elements can be added onto a subject at runtime
-    private val weatherSubject: BehaviorSubject<Weather> = BehaviorSubject.create()
+    private val behaviorSubject: BehaviorSubject<MutableState> = BehaviorSubject.create()
 
     // Not mutable, we are subscribing to this as state in the view
-    val weatherObservable: Observable<Weather> = weatherSubject
+    val homeObservable: Observable<MutableState> = behaviorSubject
 
     private val loadingSubject: PublishSubject<Boolean> = PublishSubject.create()
     val loadingObservable: Observable<Boolean> = loadingSubject
+
+    private val currentMutableState
+        get() = behaviorSubject.value ?: MutableState(
+            weather = Weather(),
+            memoir = UserInfo.Memoir(
+                question = "What is your earliest memory?",
+                thought = "The most vivid memory I had when I was a kid traces back to the year I turned 3. I was in the park playing with puppies...",
+                creationTime = "Feb 8, 2023",
+                mainWeatherConditionIcon = "04n",
+                mainWeatherConditionDescription = ""
+            )
+        )
+
+    data class MutableState(
+        val weather: Weather,
+        val memoir: UserInfo.Memoir
+    )
 
     fun getWeather(lat: String, lon: String) {
         loadingSubject.onNext(true)
@@ -36,12 +54,12 @@ class HomeViewModel(
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                     {
-                        weatherSubject.onNext(it)
+                        behaviorSubject.onNext(currentMutableState.copy(weather = it))
                         loadingSubject.onNext(false)
                     },
                     {
                         loadingSubject.onNext(false)
-                        Log.e(Constants.MAIN_TAG, "Error subscribing to getWeather()", it)
+                        Log.e(Constants.HOME_TAG, "Error subscribing to getWeather()", it)
                     }
                 )
         )
